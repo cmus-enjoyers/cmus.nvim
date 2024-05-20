36de68ea75680aa17d83b7a13cf26ab08fea6380
VKTRenokh <viktorenokh@gmail.com>
2024-05-20 21:54:59 +0300

    SlikeDollar

--- lua/cmus/init.lua
+++ lua/cmus/init.lua
@@ -1,7 +1,13 @@
-local hasTelescope, telescope = pcall(require, "telescope")
+local hasTelescope = pcall(require, "telescope")
+local hasPlenary = pcall(require, "plenary")
 
 if not hasTelescope then
 	vim.notify("you should have telescope installed", vim.log.levels.ERROR)
+	return
+end
+
+if not hasPlenary then
+	vim.notify("You should have plenary installed", vim.log.levels.ERROR)
 	return
 end
 
--- lua/cmus/plugin.lua
+++ lua/cmus/plugin.lua
@@ -1,23 +1,33 @@
+local scan = require("plenary.scandir")
+
 M = {}
+
+function M.scan(path)
+	return scan.scan_dir(path, { depth = 1, hidden = true, add_dirs = true })
+end
 
 local defaultOpts = {}
 
-local a
+--- @param filePath string - path to file that will be played using cmus-remote
+function M.play(filePath)
+	local succes = os.execute("/usr/bin/cmus-remote -f ~/music/" .. filePath)
 
---- @param input string - string to split
---- @param sep string - string separator
-function M.split(input, sep)
-	if sep == nil then
-		sep = "%s"
+	if not succes then
+		vim.notify("Cannot play " .. filePath, vim.log.levels.ERROR)
+		return false
 	end
 
-	local t = {}
+	vim.notify("Playing " .. filePath, vim.log.levels.INFO)
 
-	for str in string.gmatch(input, "([^" .. sep .. "]+)") do
-		table.insert(t, str)
-	end
+	return true
+end
 
-	return t
+function M.entry_maker(entry)
+	return {
+		value = entry:gsub("% ", "\\ "),
+		display = entry,
+		ordinal = entry,
+	}
 end
 
 --- @param opts table - cmus.nvim options
@@ -29,9 +39,9 @@ function M.setup(opts)
 	local finders = require("telescope.finders")
 	local conf = require("telescope.config").values
 	local actions = require("telescope.actions")
-	local action_state = require("telescope.")
+	local action_state = require("telescope.actions.state")
 
-	local music = io.popen("dir ~/music/vk_____________treenokh/")
+	local music = M.scan("/home/vktrenokh/music/vk_____________treenokh")
 
 	if not music then
 		vim.notify("cannot open music folder", vim.log.levels.ERROR)
@@ -42,14 +52,20 @@ function M.setup(opts)
 		.new({}, {
 			prompt_title = "Cmus song picker",
 			finder = finders.new_table({
-				results = M.split(music:read("a"), "\n"),
+				results = music,
+				entry_maker = M.entry_maker,
 			}),
 			sorter = conf.generic_sorter({}),
 			attach_mappings = function(prompt_bufnr, map)
 				actions.select_default:replace(function()
 					actions.close(prompt_bufnr)
 					local selection = action_state.get_selected_entry()
-					vim.api.nvim_put({ selection[1] }, "", false, true)
+
+					for k, v in pairs(selection) do
+						vim.notify(v .. k, vim.log.levels.INFO)
+					end
+
+					M.play(selection[2])
 				end)
 				return true
 			end,
